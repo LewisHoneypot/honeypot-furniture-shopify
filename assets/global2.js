5be790dcb89498e1222794b0f693c74e57c18376
Lewis <125884990+LewisHoneypot@users.noreply.github.com>
2024-08-20 15:58:53 +0100

    separate AnchorTarget js

--- assets/global2.js
+++ assets/global2.js
@@ -767,25 +767,3 @@ class CategorySlide extends HTMLElement {
   }
 }
 customElements.define("category-slide", CategorySlide);
-
-class AnchorTarget extends HTMLElement {
-  constructor() {
-    super();
-
-    const buttons = document.querySelectorAll("[data-anchor]");
-
-    if (!buttons) return;
-    buttons.forEach((button) => {
-      const section = button.closest("[data-anchor-container]"),
-        sectionHeight = section.offsetHeight;
-
-      button.addEventListener("click", () => {
-        window.scrollTo({
-          top: sectionHeight,
-          behavior: "smooth",
-        });
-      });
-    });
-  }
-}
-customElements.define("anchor-target", AnchorTarget);
