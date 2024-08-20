63056011a3a61d8566f949f79b8774d38e30233a
Lewis <125884990+LewisHoneypot@users.noreply.github.com>
2024-08-20 15:10:20 +0100

    separate Parallax js

--- assets/global2.js
+++ assets/global2.js
@@ -1996,32 +1996,3 @@ class WriteComment extends HTMLElement {
 }
 customElements.define("write-comment", WriteComment);
 
-class Parallax extends HTMLElement {
-  constructor() {
-    super();
-
-    this.parallax = this.querySelector("[data-parallax]");
-
-    this.init(this.parallax);
-  }
-
-  init(item) {
-    let event = item,
-      ctn = event.closest("[data-parallax-container]");
-
-    event.style.transition = "0s";
-    gsap.set(event, { yPercent: 0 });
-    gsap.to(event, {
-      yPercent: 25,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ctn,
-        start: "top 0%",
-        end: "bottom top",
-        scrub: 0.5,
-      },
-    });
-  }
-}
-
-customElements.define("parallax-container", Parallax);
