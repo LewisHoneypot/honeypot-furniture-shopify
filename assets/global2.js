7c3e7ff25906b645e2318ee191f9f935d1d2dc1b
Lewis <125884990+LewisHoneypot@users.noreply.github.com>
2024-08-20 15:17:14 +0100

    separate ProductCustomerViewing js

--- assets/global2.js
+++ assets/global2.js
@@ -1946,31 +1946,3 @@ class VideoTemplateOpener extends HTMLElement {
   }
 }
 customElements.define("video-template-opener", VideoTemplateOpener);
-
-class ProductCustomerViewing extends HTMLElement {
-  constructor() {
-    super();
-
-    const wrapper = document.querySelector(".product__quickview-inner");
-
-    if (wrapper) {
-      const numbersViewer = wrapper.getAttribute("data-customer-view"),
-        numbersViewerList = JSON.parse("[" + numbersViewer + "]"),
-        numbersViewerTime = wrapper.getAttribute("data-customer-view-time"),
-        timeViewer = parseInt(numbersViewerTime) * 1000;
-
-      setInterval(function () {
-        const numbersViewerItem = Math.floor(
-          Math.random() * numbersViewerList.length
-        );
-
-        wrapper.querySelector(".text").innerHTML =
-          window.customer_view.text.replace(
-            "[number]",
-            numbersViewerList[numbersViewerItem]
-          );
-      }, timeViewer);
-    }
-  }
-}
-customElements.define("customer-viewing", ProductCustomerViewing);
