129ee540d15dcf833bb48837653d232a724ec4c3
Lewis <125884990+LewisHoneypot@users.noreply.github.com>
2024-08-20 14:50:08 +0100

    separate gallery js

--- assets/global2.js
+++ assets/global2.js
@@ -2142,24 +2142,3 @@ class SplittingAnimate extends HTMLElement {
   }
 }
 customElements.define("splitting-animate", SplittingAnimate);
-
-// gallery js
-const gallery = document.querySelectorAll(".gallery-js");
-
-gallery.forEach((i) => {
-  const id = i.getAttribute("id");
-
-  window.lightGallery(document.querySelector(`.gallery-js#${id}`), {
-    selector: ".item-gallery",
-    autoplayFirstVideo: false,
-    pager: false,
-    galleryId: "nature",
-    plugins: [lgThumbnail],
-    mobileSettings: {
-      controls: false,
-      showCloseIcon: false,
-      download: false,
-      rotate: false,
-    },
-  });
-});
