598eaf7a06da1f85d1f8180b2dfbdb7e49dec437
Lewis <125884990+LewisHoneypot@users.noreply.github.com>
2024-10-14 10:18:04 +0100

    update video id hides

--- assets/productPage.js
+++ assets/productPage.js
@@ -1,15 +1,27 @@
-// Hide missing product videos
+// Hide missing product videos for QM4Cg7 or FxQj6U
 if (typeof productVideo !== "undefined" && productVideo === "") {
-  let productVideoElement = document.querySelector('[id*="QM4Cg7"], [id*="FxQj6U"]');
-  if (productVideoElement) {
-    productVideoElement.style.display = "none";
+  let productVideoElementQM4Cg7 = document.querySelector('[id*="QM4Cg7"]');
+  let productVideoElementFxQj6U = document.querySelector('[id*="FxQj6U"]');
+  
+  if (productVideoElementQM4Cg7) {
+    productVideoElementQM4Cg7.style.display = "none";
+  }
+  
+  if (productVideoElementFxQj6U) {
+    productVideoElementFxQj6U.style.display = "none";
   }
 }
 
-// Hide missing product videos
+// Hide missing product videos for QM4Cg7 or FxQj6U
 if (typeof productVideo !== "undefined" && productVideo === "") {
-  var element = document.querySelector('[id*="QM4Cg7"], [id*="FxQj6U"]');
-  if (element) {
-    element.style.display = "none";
+  var elementQM4Cg7 = document.querySelector('[id*="QM4Cg7"]');
+  var elementFxQj6U = document.querySelector('[id*="FxQj6U"]');
+  
+  if (elementQM4Cg7) {
+    elementQM4Cg7.style.display = "none";
+  }
+  
+  if (elementFxQj6U) {
+    elementFxQj6U.style.display = "none";
   }
 }
